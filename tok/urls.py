3c0316a2b98232b6eb01c40fd3083e9c4252210f
woodybriggs <briggswoody@gmail.com>
2018-12-13 11:50:43 +0000

    Update urls.py

--- tok/urls.py
+++ tok/urls.py
@@ -14,7 +14,7 @@ router.register(r'users', UserCreateViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('api-auth-token/', views.obtain_auth_token)
+    path('api-auth-token/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     # the 'api-root' from django rest-frameworks default router
